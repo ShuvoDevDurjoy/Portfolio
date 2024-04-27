6c8140afeb38256214c9a5f12bf4e9208ec44168
Shuvo Dev Durjoy <shuvo.dev@northsouth.edu>
2024-04-27 21:00:52 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,17 @@
-document.addEventListener("DOMContentLoaded", ()=>
-{
-    const text = document.querySelector('.text') ;
-    text.classList.add("active") ;  
-}) ; 
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.querySelector('.text');
+    text.classList.add("active");
+});
+
+const OCIcon = document.querySelector('.open-close-icon');
+const OCIconImage = document.getElementById('icon');
+
+OCIcon.addEventListener('click', () => {
+    if (OCIconImage.classList.contains('open')) {
+        OCIconImage.src = 'menu.png';
+        OCIconImage.classList.remove('open');
+    } else {
+        OCIconImage.src = 'close.png';
+        OCIconImage.classList.add('open');
+    }
+});
